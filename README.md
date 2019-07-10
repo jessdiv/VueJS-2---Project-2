356daf0e9ebf5cabd82f9d327e9df0af217fbce7
Jessica Diver <jessica.diver@painchek.com>
2019-07-10 10:34:40 +1000

    completed Project 2 of the course!

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Vue JS 2 - The Complete Guide (incl. Vue Router & Vuex)
+
+My completed Project 2 from the Udemy Course by by Maximilian Schwarzmüller
+
+[Join the course here!](https://www.udemy.com/vuejs-2-the-complete-guide/)
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-add-quote @quoteAdded="newQuote"></app-add-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './Components/QuoteGrid.vue'
+    import AddQuote from './Components/AddQuote.vue'
+    import Header from './Components/Header.vue'
+
+    export default {
+        data: function() {
+            return {
+                quotes: ['First Quote'],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+          newQuote(quote) {
+              if (this.quotes.length >= this.maxQuotes) {
+                  return alert('Please delete quotes first');
+              }
+              this.quotes.push(quote);
+          },
+          deleteQuote(index) {
+              this.quotes.splice(index, 1);
+          }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appAddQuote: AddQuote,
+            appHeader: Header
+        },
+    }
+</script>
+
+<style>
+</style>
